7bae654fda3a7d4257c445036c45a1f3e42f2c3e
Nils Norman Haukås <nils@thunki.com>
2017-07-13 22:35:00 +0200

    update frontpage text

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,8 @@ export default () =>
       <p>
         I botn ligg det bla. kommunedata i frå SSB og Difi. Meir info om
         prosjektet{' '}
-        <a href="https://github.com/nilsnh/kommunelogo">finn du her</a>. Om du
-        vil bygge vidare på rådataene så finst det eit API{' '}
-        <a href="/api.json">her</a>. :)
+        <a href="https://github.com/nilsnh/kommunelogo">finn du her</a>,
+        rådataene finn du <a href="/api.json">her</a>. :)
       </p>
       <p />
     </div>
