3f125d79261a23dc48b005b7808386e73c4d470c
Nils Norman Haukås <nils@thunki.com>
2017-07-13 18:02:52 +0200

    update frontpage text

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import MunicipalityList from './MunicipalityList'
 export default () =>
   <main className="u-margin">
     <div className="u-1/3@desktop">
-      <h1>kommunelogo. nilsnh.no</h1>
+      <h1>
+        kommunelogo. <a href="https://nilsnh.no">nilsnh.no</a>
+      </h1>
       <p>
         Det er framleis for vanskeleg å finne frem til kommunelogoar. Målet med
         denne sida er å vere ei lita oversiktsressurs for å raskt kunne hente
         ned logofiler, visuell profil o.l. for kommuner.
       </p>
       <p>
-        I botn ligg det bla. kommunedata i frå SSB og Difi. Meir info om
-        prosjektet og korleis du kan bidra finn du her.
+        I botn ligg det bla. kommunedata i frå
+        [SSB](https://data.norge.no/data/statistisk-sentralbyrå/kommuneinndeling)
+        og [Difi](https://orglogo.difi.no/api). Meir info om prosjektet [finn du
+        her](https://github.com/nilsnh/kommunelogo).
       </p>
     </div>
     <MunicipalityList />
